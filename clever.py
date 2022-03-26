a58eceef0fbe3c354a865db0195437f60b970add
Kiash254 <codewitkiash@gmil.com>
2022-03-27 00:16:05 +0300

    first  commit

---
+++ clever.py
@@ -0,0 +1,80 @@
+import turtle
+#making a square
+# qazi_turtle=turtle.Turtle()
+# qazi_turtle.forward(100)
+# qazi_turtle.right(90)
+# qazi_turtle.forward(100)
+# qazi_turtle.right(90)
+# qazi_turtle.forward(100)
+# qazi_turtle.right(90)
+# qazi_turtle.forward(100)
+# qazi_turtle.right(90)
+#function
+def sqaure():
+    qazi_turtle=turtle.Turtle()
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+   
+# sqaure()
+# turtle.forward(500)
+#if else statement
+elephant=3000
+ant=5000
+
+if (elephant>ant):
+    sqaure()
+else:
+    turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+import calendar
+
+print(calendar.weekheader(9))
+
+print()
+print(calendar.firstweekday())
+print()
+
+print(calendar.month(2022,3))
+
+
+print(calendar,calendar.monthcalendar(2022,3))
+print()
+
+print(calendar.calendar(2022))
+
+
+
+days_week=calendar.weekday(2022,3,25)
+print(days_week)
+
+is_leap=calendar.isleap(2022)
+print(is_leap)
+
+
+how_many_leap=calendar.leapdays(2020,2022)
+print(how_many_leap)
+
+import datetime
+import time
+
+
+
+
+
+
